44779c934daf369aa48793efb7176fea715625a3
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-11-25 09:25:11 +0100

    Detele obstacles

--- js/layers_IMPD.js
+++ js/layers_IMPD.js
@@ -57,70 +57,70 @@ var layers = [
       fFeature: "Type",
     },
   },
-  {
-    name: "Obstacles",
-    sourceLayerName: "IMPD_obstacles", //source-layer & source
-    attribution:
-      "Accessibility Data: <a href='https://opendata-ajuntament.barcelona.cat/data/ca/dataset/accessibilitat-via-publica'>©Ajuntament de Barcelona</a> (<a href='https://creativecommons.org/licenses/by/4.0/deed.ca'>CC BY 4.0</a>)",
-    sourceType: "geojson",
-    layerType: "circle",
-    symbolization: {      
-      "circle-radius": 5,
-      // "circle-stroke-width": 1,
-      // "circle-stroke-color": "#333",
-      "circle-color": [
-        "match",
-        ["get", "Evaluation"],   // property name in your GeoJSON
-        "Light", "#F6CF71",
-        "Moderate", "#F89C74",
-        "Severe", "#F03B20",
-        /* default color */ "hsl(0, 0%, 70%)"
-      ]
+  // {
+  //   name: "Obstacles",
+  //   sourceLayerName: "IMPD_obstacles", //source-layer & source
+  //   attribution:
+  //     "Accessibility Data: <a href='https://opendata-ajuntament.barcelona.cat/data/ca/dataset/accessibilitat-via-publica'>©Ajuntament de Barcelona</a> (<a href='https://creativecommons.org/licenses/by/4.0/deed.ca'>CC BY 4.0</a>)",
+  //   sourceType: "geojson",
+  //   layerType: "circle",
+  //   symbolization: {      
+  //     "circle-radius": 5,
+  //     // "circle-stroke-width": 1,
+  //     // "circle-stroke-color": "#333",
+  //     "circle-color": [
+  //       "match",
+  //       ["get", "Evaluation"],   // property name in your GeoJSON
+  //       "Light", "#F6CF71",
+  //       "Moderate", "#F89C74",
+  //       "Severe", "#F03B20",
+  //       /* default color */ "hsl(0, 0%, 70%)"
+  //     ]
 
-    },
-    legend: {
-      id: "legend-a",
-      class: "legend",
-      items: [
-    {
-      styleHeight: "12px",
-      display: "inline-block",
-      backgroundColor: "#F6CF71",
-      range: ["Light"]
-    },
-    {
-      styleHeight: "12px",
-      display: "inline-block",
-      backgroundColor: "#F89C74",
-      range: ["Moderate"]
-    },
-    {
-      styleHeight: "12px",
-      display: "inline-block",
-      backgroundColor: "#F03B20",
-      range: ["Severe"]
-    }
-      ],
-    },
-    states: {
-      visible: "visible",
-      popUps: true,
-      icons: true,
-      filterCat: true,
-      highlight: false,
-      filterLayer: false,
-      dateRange: false,
-    },
-    popUpFeatures: {
-      event: "click",
-      fields: ["Type", "Evaluation", "Value"]
-      // img: "image" // optional, you can add later if needed
-    },
-    filterBy: {
-      active: false,
-      fFeature: "Type",
-    },
-  },
+  //   },
+  //   legend: {
+  //     id: "legend-a",
+  //     class: "legend",
+  //     items: [
+  //   {
+  //     styleHeight: "12px",
+  //     display: "inline-block",
+  //     backgroundColor: "#F6CF71",
+  //     range: ["Light"]
+  //   },
+  //   {
+  //     styleHeight: "12px",
+  //     display: "inline-block",
+  //     backgroundColor: "#F89C74",
+  //     range: ["Moderate"]
+  //   },
+  //   {
+  //     styleHeight: "12px",
+  //     display: "inline-block",
+  //     backgroundColor: "#F03B20",
+  //     range: ["Severe"]
+  //   }
+  //     ],
+  //   },
+  //   states: {
+  //     visible: "visible",
+  //     popUps: true,
+  //     icons: true,
+  //     filterCat: true,
+  //     highlight: false,
+  //     filterLayer: false,
+  //     dateRange: false,
+  //   },
+  //   popUpFeatures: {
+  //     event: "click",
+  //     fields: ["Type", "Evaluation", "Value"]
+  //     // img: "image" // optional, you can add later if needed
+  //   },
+  //   filterBy: {
+  //     active: false,
+  //     fFeature: "Type",
+  //   },
+  // },
   {
     name: "Slope",
     sourceLayerName: "IMPD_slope", //source-layer & source
